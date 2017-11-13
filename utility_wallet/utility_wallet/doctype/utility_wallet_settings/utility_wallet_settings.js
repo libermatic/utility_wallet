fc542b6cb7ca6196f504187a7d52451bb01a7b96
Sun Howwrongbum <sun@adrenosapient.com>
2017-11-14 02:34:55 +0530

    Add doctypes

---
+++ utility_wallet/utility_wallet/doctype/utility_wallet_settings/utility_wallet_settings.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2017, Libermatic and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Utility Wallet Settings', {
+	refresh: function(frm) {
+
+	}
+});
