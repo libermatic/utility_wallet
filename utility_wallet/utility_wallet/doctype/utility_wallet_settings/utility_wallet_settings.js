a925ce8b5a1c05cfc90e3335c8cc4caedb1a2648
Sun Howwrongbum <sun@adrenosapient.com>
2017-11-18 17:37:23 +0530

    Filter Utility Wallet Settings docfield select values

--- utility_wallet/utility_wallet/doctype/utility_wallet_settings/utility_wallet_settings.js
+++ utility_wallet/utility_wallet/doctype/utility_wallet_settings/utility_wallet_settings.js
@@ -1,8 +1,53 @@
 // Copyright (c) 2017, Libermatic and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on('Utility Wallet Settings', {
-	refresh: function(frm) {
+cur_frm.fields_dict['wallet_account'].get_query = function(doc) {
+  return {
+    filters: {
+      account_type: 'Stock',
+      is_group: false,
+    },
+  };
+};
 
-	}
-});
+cur_frm.fields_dict['sale_income_account'].get_query = function(doc) {
+  return {
+    filters: {
+      root_type: 'Income',
+      is_group: false,
+    },
+  };
+};
+cur_frm.fields_dict['sale_charges_account'].get_query = function(doc) {
+  return {
+    filters: {
+      root_type: 'Expense',
+      is_group: false,
+    },
+  };
+};
+cur_frm.fields_dict['debit_to'].get_query = function(doc) {
+  return {
+    filters: {
+      account_type: 'Cash',
+      is_group: false,
+    },
+  };
+};
+
+cur_frm.fields_dict['purchase_commission_account'].get_query = function(doc) {
+  return {
+    filters: {
+      root_type: 'Income',
+      is_group: false,
+    },
+  };
+};
+cur_frm.fields_dict['credit_from'].get_query = function(doc) {
+  return {
+    filters: {
+      account_type: 'Bank',
+      is_group: false,
+    },
+  };
+};
