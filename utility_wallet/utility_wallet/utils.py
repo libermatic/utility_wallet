b065e917c4652538e86bb2fe7e081302eacc3883
Sun Howwrongbum <sun@adrenosapient.com>
2017-11-24 07:05:16 +0530

    Add whitelisted function

--- utility_wallet/utility_wallet/utils.py
+++ utility_wallet/utility_wallet/utils.py
@@ -8,9 +8,25 @@ from frappe.utils import flt
 
 @frappe.whitelist()
 def get_wallet_balance(wallet_provider=None, wallet_account=None):
+    cond = []
+    if wallet_provider:
+        cond.append("against = '{}'".format(wallet_provider))
+    if wallet_account:
+        cond.append("account = '{}'".format(wallet_account))
+    else:
+        cond.append("account = '{}'".format(frappe.db.get_value('Utility Wallet Settings', None, 'wallet_account')))
     bal = frappe.db.sql("""
             SELECT sum(debit) - sum(credit)
             FROM `tabGL Entry`
-            WHERE account = '{0}' AND against = '{1}'
-        """.format(wallet_account, wallet_provider))[0][0]
+            WHERE {}
+        """.format(" and ".join(cond)))[0][0]
     return flt(bal)
+
+@frappe.whitelist()
+def get_all_wallet_balances():
+    data = []
+    wallets = frappe.get_list("Supplier", fields=["name"], filters = { 'has_wallet': True })
+    for w in wallets:
+        data.append({ 'wallet_provider': w.name, 'balance': get_wallet_balance(w.name) })
+
+    return data
