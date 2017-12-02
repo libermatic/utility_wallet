884c2756ae7fb8eb40a756a8f091c7762ab10906
Sun Howwrongbum <sun@adrenosapient.com>
2017-12-03 00:24:46 +0530

    Add status indicator t Utility Sale

--- utility_wallet/utility_wallet/doctype/utility_sale/utility_sale_list.js
+++ utility_wallet/utility_wallet/doctype/utility_sale/utility_sale_list.js
@@ -1,4 +1,11 @@
 frappe.listview_settings['Utility Sale'] = {
+  add_fields: ['total', 'paid_amount'],
+  get_indicator: function(doc) {
+    if (flt(doc['total']) === flt(doc['paid_amount'])) {
+      return [__('Paid'), 'green', 'total,=,paid_amount'];
+    }
+    return [__('Pending'), 'orange', 'total,>,paid_amount'];
+  },
   reports: [
     {
       name: 'Wallet Balance',
