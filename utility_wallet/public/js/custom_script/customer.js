7b49aaa29b5784dffef0006db3499a8783b57ce4
Sun Howwrongbum <sun@libermatic.com>
2018-03-28 23:54:22 +0530

    Customer: fix custom button appearing twice

--- utility_wallet/public/js/custom_script/customer.js
+++ utility_wallet/public/js/custom_script/customer.js
@@ -1,6 +1,11 @@
 frappe.ui.form.on('Customer', 'refresh', function(frm) {
-  if (frm.doc.utility_service.length > 0) {
-    frm.add_custom_button(__('Make Utility Sale'), function() {
+  const button_label = 'Make Utility Sale';
+  if (
+    !frm.doc.__islocal &&
+    !frm.custom_buttons[button_label] &&
+    frm.doc.utility_service.length > 0
+  ) {
+    frm.add_custom_button(__(button_label), function() {
       frm.make_new('Utility Sale');
     });
   }
